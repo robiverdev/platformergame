1ac0fdfd90456261b4223a5fb252665ff3d87085
robiverdev <robi.verdev1@gmail.com>
2025-08-28 17:07:22 +0200

    add collision classes, player dies when touches danger

--- main.lua
+++ main.lua
@@ -1,23 +1,39 @@
 function love.load()
   wf = require "libraries/windfield/windfield" -- Windfield lib
-  world = wf.newWorld(0, 800) -- Creating a "world" with gravity
+  world = wf.newWorld(0, 800, false) -- Creating a "world" with gravity
 
-  player = world:newRectangleCollider(360, 100, 80, 80) -- Collider = physics object
+  -- Define "Platform" first so that "Player" can safely reference it in its ignores list
+  world:addCollisionClass("Platform")
+  world:addCollisionClass("Player"--[[, {ignores = {"Platform"}}]])
+  world:addCollisionClass("Danger")
+ 
+  player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "Player"}) -- Collider = physics object
+  player:setFixedRotation(true)
   player.speed = 240
 
-  platform = world:newRectangleCollider(250, 400, 300, 100)
+  platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "Platform"})
   platform:setType("static")
+
+  dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = "Danger"})
+  dangerZone:setType("static")
 end
 
 function love.update(dt)
   world:update(dt)
 
-  local  px, py = player:getPosition()
-  if love.keyboard.isDown("right") then
-    player:setX(px + player.speed * dt)
-  end
-  if love.keyboard.isDown("left") then
-    player:setX(px - player.speed * dt)
+  if player.body then
+    local  px, py = player:getPosition()
+    if love.keyboard.isDown("right") then
+      player:setX(px + player.speed * dt)
+    end
+
+    if love.keyboard.isDown("left") then
+      player:setX(px - player.speed * dt)
+    end
+
+    if player:enter("Danger") then
+      player:destroy()
+    end
   end
 end
 
