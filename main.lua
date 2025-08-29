036a8cd0065d03b06553b548d9b4688d03a09b34
robiverdev <robi.verdev1@gmail.com>
2025-08-29 17:06:57 +0200

    cleaned code a bit and added extra sprites

--- main.lua
+++ main.lua
@@ -19,15 +19,9 @@ function love.load()
   world:addCollisionClass("Platform")
   world:addCollisionClass("Player"--[[, {ignores = {"Platform"}}]])
   world:addCollisionClass("Danger")
- 
-  player = world:newRectangleCollider(360, 100, 40, 100, {collision_class = "Player"}) -- Collider = physics object
-  player:setFixedRotation(true)
-  player.speed = 240
-  player.animation = animations.idle
-  player.isMoving = false
-  player.direction = 1 -- 1 right, -1 left
-  player.grounded = true
 
+  require("player")
+ 
   platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "Platform"})
   platform:setType("static")
 
@@ -37,53 +31,12 @@ end
 
 function love.update(dt)
   world:update(dt)
-
-  if player.body then
-    local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 40, 2, {"Platform"})
-
-    if #colliders > 0 then 
-      player.grounded = true
-    else 
-      player.grounded = false
-    end
-
-    player.isMoving = false
-    local  px, py = player:getPosition()
-    if love.keyboard.isDown("right") then
-      player:setX(px + player.speed * dt)
-      player.isMoving = true
-      player.direction = 1
-    end
-
-    if love.keyboard.isDown("left") then
-      player:setX(px - player.speed * dt)
-      player.isMoving = true
-      player.direction = -1
-    end
-
-    if player:enter("Danger") then
-      player:destroy()
-    end
-  end
-
-  if player.grounded then
-    if player.isMoving then
-      player.animation = animations.run
-    else 
-      player.animation = animations.idle
-    end
-  else
-    player.animation = animations.jump
-  end
-
-  player.animation:update(dt)
+  playerUpdate(dt)
 end
 
 function love.draw()
   world:draw()
-
-  local px, py = player:getPosition()
-  player.animation:draw(sprites.playerSheet, px, py, nil, 0.25 * player.direction, 0.25, 130, 300)
+  drawPlayer()
 end
 
 function love.keypressed(key)
