12be7c58310428ab81ae51bb8483729c7e8ec64a
robiverdev <robi.verdev1@gmail.com>
2025-08-30 13:20:57 +0200

    added transitioning between levels with a flag, added saving game option

--- main.lua
+++ main.lua
@@ -31,13 +31,24 @@ function love.load()
 
   require("player")
   require("enemy")
+  require("libraries/show")
 
   -- dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = "Danger"})
   -- dangerZone:setType("static")
 
   platforms = {}
 
-  loadMap()
+  flagX = 0
+  flagY = 0
+
+  saveData = {}
+  saveData.currentLevel = "level1"
+  if love.filesystem.getInfo("savegame.lua") then
+    local data = love.filesystem.load("savegame.lua")
+    data()
+  end
+
+  loadMap(saveData.currentLevel)
 end
 
 function love.update(dt)
@@ -48,6 +59,15 @@ function love.update(dt)
 
   local px, py = player:getPosition()
   cam:lookAt(px, love.graphics.getHeight() / 2)
+
+  local colliders = world:queryCircleArea(flagX, flagY, 10, {"Player"})
+  if #colliders > 0 then
+    if saveData.currentLevel == "level1" then
+      loadMap("level2")
+    elseif saveData.currentLevel == "level2" then
+      loadMap("level1")
+    end
+  end
 end
 
 function love.draw()
@@ -64,6 +84,10 @@ function love.keypressed(key)
     if player.grounded then
       player:applyLinearImpulse(0, -4000)
     end
+  end
+
+  if key == "r" then
+    loadMap("level2")
   end
 end
 
@@ -84,13 +108,42 @@ function spawnPlatform(x, y, width, height)
   end
 end
 
+function destroyAll()
+  local i = #platforms
+  while i > -1 do
+    if platforms[i] ~= nil then
+      platforms[i]:destroy()
+    end
+    table.remove(platforms, i)
+    i = i -1
+  end
+
+   local i = #enemies
+  while i > -1 do
+    if enemies[i] ~= nil then
+      enemies[i]:destroy()
+    end
+    table.remove(enemies, i)
+    i = i -1
+  end
+end
+
 function loadMap(mapName)
-  gameMap = sti("maps/level1.lua")
+  saveData.currentLevel = mapName
+  love.filesystem.write("savegame.lua", table.show(saveData, "saveData"))
+  destroyAll()
+  player:setPosition(300, 100)
+  gameMap = sti("maps/" .. mapName .. ".lua")
   for i, obj in pairs(gameMap.layers["Platforms"].objects) do
     spawnPlatform(obj.x, obj.y, obj.width, obj.height)
   end
 
   for i, obj in pairs(gameMap.layers["Enemies"].objects) do
     spawnEnemy(obj.x, obj.y)
+  end
+
+   for i, obj in pairs(gameMap.layers["Flag"].objects) do
+    flagX = obj.x
+    flagY = obj.y
   end
 end
